9f714d43c6f40a76d77a4566554f76a719ab5e8b
Rathi2606 <rathipriya1d26@gmail.com>
2025-12-31 18:57:33 +0530

    update in api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import { createApi,fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const Backend_URL="http://localhost:8000";
+const Backend_URL="https://it-developer-be-rathi.onrender.com";
 
 export const api = createApi({
     reducerPath: "api",
